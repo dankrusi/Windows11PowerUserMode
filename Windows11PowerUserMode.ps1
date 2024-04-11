c6a99afb0554247c5defe5e83b4d0e4bad5ab43b
Dan Krusi <dan@nerves.ch>
2024-04-11 11:22:57 +0200

    initial commit

---
+++ Windows11PowerUserMode.ps1
@@ -0,0 +1,53 @@
+# Get original path
+$originalPWD=$PWD
+
+# Get the ID and security principal of the current user account
+ $myWindowsID=[System.Security.Principal.WindowsIdentity]::GetCurrent()
+ $myWindowsPrincipal=new-object System.Security.Principal.WindowsPrincipal($myWindowsID)
+
+
+ # Get the security principal for the Administrator role
+ $adminRole=[System.Security.Principal.WindowsBuiltInRole]::Administrator
+
+ # Check to see if we are currently running "as Administrator"
+ if ($myWindowsPrincipal.IsInRole($adminRole))
+    {
+    # We are running "as Administrator" - so change the title and background color to indicate this
+    $Host.UI.RawUI.WindowTitle = $myInvocation.MyCommand.Definition + "(Elevated)"
+    $Host.UI.RawUI.BackgroundColor = "DarkBlue"
+    clear-host
+    }
+ else
+    {
+    # We are not running "as Administrator" - so relaunch as administrator
+
+    # Create a new process object that starts PowerShell
+    $newProcess = new-object System.Diagnostics.ProcessStartInfo "PowerShell";
+
+    # Specify the current script path and name as a parameter
+    $newProcess.Arguments = $myInvocation.MyCommand.Definition;
+
+    # Indicate that the process should be elevated
+    $newProcess.Verb = "runas";
+
+    # Start the new process
+    [System.Diagnostics.Process]::Start($newProcess);
+
+    # Exit from the current, unelevated, process
+    exit
+    }
+Set-Location (Split-Path -Path $myInvocation.MyCommand.Definition)
+
+
+
+
+ # Run your code that needs to be elevated here
+ Pause
+
+#Remove change desktop anim
+.\Utils\ViVeTool-v0.3.3\ViVeTool.exe /disable /id:42354458
+ 
+#Remove change desktop label
+.\Utils\ViVeTool-v0.3.3\ViVeTool.exe /disable /id:34508225,40459297
+
+Pause
